2e9e205d4713927c9e0ff263b1bbeb1d9125d32a
Bleyle823 <bennybleylle09@gmail.com>
2025-08-28 18:52:11 +0300

    Update outputFileTracingRoot in next.config.mjs to use process.cwd() for accurate build tracing

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracingRoot: __dirname,
+  outputFileTracingRoot: process.cwd(),
   // Silence warnings
   // https://github.com/WalletConnect/walletconnect-monorepo/issues/1908
   webpack: (config) => {
